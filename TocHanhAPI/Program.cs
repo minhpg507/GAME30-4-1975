9d845d05761b4b05fa61ea314747d36df5e2dd08
Ha Duc Minh <haduc.min507@gmail.com>
2026-08-15 13:59:24 +0700

    Configure database and CORS in Program.cs
    Added database configuration and CORS policy to the application.

--- TocHanhAPI/Program.cs
+++ TocHanhAPI/Program.cs
@@ -3,26 +3,20 @@ using TocHanhAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// 1. THÊM CẤU HÌNH DATABASE VÀ CORS (TRƯỚC khi Build)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors(options => options.AddPolicy("AllowAll",
     p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
-// ---------------------------------------------------
 
 var app = builder.Build();
 
-// 2. KÍCH HOẠT CORS (NGAY SAU khi Build)
 app.UseCors("AllowAll");
-// --------------------------------------
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
